0735b4dea4debab31438a4162f7ef28023ca1868
Deniz Akşimşek <deniz@denizaksimsek.com>
2023-10-12 02:22:19 +0300

    Fix metadata showing in file manager

--- lib/storage.ts
+++ lib/storage.ts
@@ -4,18 +4,19 @@ import * as path from "https://deno.land/std@0.203.0/path/mod.ts"
 
 export const read = async (name: string): Promise<Blob> =>
 	new Blob([await Deno.readFile(encode(name))], {
-		type: await Deno.readTextFile(encode(name) + ".type")
+		type: await Deno.readTextFile(encode(name) + ".#type")
 	})
 
 export const write = async (name: string, blob: Blob): Promise<void> => {
     await Deno.mkdir(blobDir, { recursive: true })
-    await Deno.writeTextFile(encode(name) + ".type", blob.type)
+    await Deno.writeTextFile(encode(name) + ".#type", blob.type)
     return Deno.writeFile(encode(name), blob.stream())
 }
 export const list = async function* () {
     await Deno.mkdir(blobDir, { recursive: true })
 	for await (const entry of Deno.readDir(blobDir)) {
-		yield decodeURIComponent(entry.name).split("#")[1]
+		if (entry.name.endsWith(".#type")) continue
+		yield decodeURIComponent(entry.name)
 	}
 }
 export const del = (name: string) => Deno.remove(encode(name))
