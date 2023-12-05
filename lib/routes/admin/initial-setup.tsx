d749cd3c79bc5b29020cfe3309963cd3b5ecb6f4
Deniz Akşimşek <deniz@denizaksimsek.com>
2023-12-05 17:12:47 +0300

    deno fmt

--- lib/routes/admin/initial-setup.tsx
+++ lib/routes/admin/initial-setup.tsx
@@ -7,7 +7,12 @@ import { Layout } from '../../layout.ts'
 
 import * as bcrypt from '../../../deps/bcrypt.ts'
 
-import { completeInitialSetup, createUser, initialSetupDone, setConfig } from '../../db.ts'
+import {
+	completeInitialSetup,
+	createUser,
+	initialSetupDone,
+	setConfig,
+} from '../../db.ts'
 import { User } from '../../model/user.ts'
 import { Card } from '../../model/card.ts'
 import { isValidUrl } from '../../common/util.ts'
@@ -38,15 +43,17 @@ export const post = async (c: hono.Context<Env>) => {
 	if (typeof pw !== 'string') {
 		return c.html(<InitialSetup error='Missing username or password' />, 400)
 	}
-	
+
 	const displayName = form.get('name')
 	if (typeof displayName !== 'string') {
 		return c.html(<InitialSetup error='Please enter a name' />, 400)
 	}
 
 	const siteUrl = form.get('site-url')
-	if (typeof siteUrl !== 'string'
-		|| !isValidUrl(siteUrl)) {
+	if (
+		typeof siteUrl !== 'string' ||
+		!isValidUrl(siteUrl)
+	) {
 		return c.html(<InitialSetup error='Site URL should be a valid URL' />, 400)
 	}
 
