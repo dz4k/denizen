3c9bf6a74ebcf6e24374e910e2ef2e061ce2c3a1
Deniz Akşimşek <deniz@aksimsek.tr>
2025-02-05 12:13:11 +0300

    release.sh: add container build

--- tool/release.sh
+++ tool/release.sh
@@ -5,18 +5,24 @@ die() { echo "$@" >&2; exit 1; }
 
 test ! -z "${RELEASE:-}" || die "RELEASE env. var. is undefined"
 command -v fj >/dev/null || die "forgejo-cli (fj) not installed"
+RELEASE=${RELEASE#v}
 
 # Compile
 sh tool/compile.sh
 
 # Create commit
-jj new -m "$RELEASE"
+jj new -m "v$RELEASE"
 jj bookmark move main --to @
 
 # Create release
 fj release --repo codeberg.org/dz4k/denizen create \
-  --create-tag "$RELEASE" \
+  --create-tag "v$RELEASE" \
   --draft \
   $([[ "$RELEASE" =~ "-devel" ]] && echo --prerelease) \
   $(find build -name 'denizen-*' -exec echo --attach {} \;) \
-  "Release $RELEASE"
+  "Release v$RELEASE"
+
+# Build container
+podman build . -t denizen:$RELEASE
+podman push denizen:$RELEASE codeberg.org/dz4k/denizen:$RELEASE
+podman push denizen:$RELEASE codeberg.org/dz4k/denizen:latest
